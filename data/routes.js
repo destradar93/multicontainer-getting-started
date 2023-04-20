a5a44f99b4c4b13a3ce982e4fdd50f36041d1981
Diago Estrada <estradadiago93@gmail.com>
2023-04-20 16:01:48 -0400

    Add endpoint to return current Memory info (/data/memory)

--- data/routes.js
+++ data/routes.js
@@ -9,4 +9,13 @@ router.get('/data/cpu', (req, res) => {
   res.json({ cpuLoad: cpuLoad });
 });
 
+router.get('/data/memory', (req, res) => {
+  const totalMemory = os.totalmem();
+  const freeMemory = os.freemem();
+  const usedMemory = totalMemory - freeMemory;
+  const percentageUsed = (usedMemory / totalMemory) * 100;
+
+  res.json({ totalMemory, freeMemory, usedMemory, percentageUsed });
+});
+
 module.exports = router;
